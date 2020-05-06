64c85e4803f0b3c7b186c792816c3892e1a79a51
Xin Tao <xintao0202@gmail.com>
2020-05-05 21:53:43 -0700

    Add files via upload

---
+++ Data Modeling/Project 1 Data Modeling with Postgres/sql_queries.py
@@ -0,0 +1,121 @@
+# DROP TABLES
+
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
+
+# CREATE TABLES
+
+"""
+Specify all columns for each of the five tables with the right data types and conditions
+songplay_id Using PostgreSQL SERIAL To Create Auto-increment Column
+
+"""
+songplay_table_create = ("""
+    CREATE TABLE songplays (
+        songplay_id SERIAL PRIMARY KEY,
+        start_time TIMESTAMP NOT NULL,
+        user_id VARCHAR NOT NULL,
+        level VARCHAR,
+        song_id VARCHAR NOT NULL,
+        artist_id VARCHAR NOT NULL,
+        session_id INT,
+        location VARCHAR,
+        user_agent VARCHAR 
+    );
+""")
+
+user_table_create = ("""
+CREATE TABLE users (
+        user_id VARCHAR NOT NULL PRIMARY KEY,
+        first_name VARCHAR NOT NULL,
+        last_name VARCHAR NOT NULL,
+        gender CHAR,
+        level VARCHAR NOT NULL);
+""")
+
+song_table_create = ("""
+ CREATE TABLE songs (
+        song_id VARCHAR NOT NULL PRIMARY KEY,
+        title VARCHAR NOT NULL,
+        artist_id VARCHAR NOT NULL,
+        year INT,
+        duration FLOAT);
+""")
+
+artist_table_create = ("""
+CREATE TABLE artists (
+        artist_id VARCHAR PRIMARY KEY,
+        name VARCHAR NOT NULL,
+        location VARCHAR,
+        latitude FLOAT,
+        longitude FLOAT);
+""")
+
+time_table_create = ("""
+CREATE TABLE time (
+        start_time TIMESTAMP PRIMARY KEY,
+        hour INT NOT NULL,
+        day INT NOT NULL,
+        week INT NOT NULL,
+        month INT NOT NULL,
+        year INT NOT NULL,
+        weekday INT NOT NULL);
+""")
+
+# INSERT RECORDS
+
+"""
+For the user_table_insert DO UPDATE when there is a conflict. The level field is used for free and paid services for a user. Consider someone subscribes by paying a fee who happened to be a free user before. In that scenario, we would like to update the level of the user to paid. `ON CONFLICT (user_id) DO UPDATE SET level=EXCLUDED.level`
+
+"""
+
+songplay_table_insert = ("""
+ INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+    VALUES(to_timestamp(%s), %s, %s, %s, %s, %s, %s, %s);
+""")
+
+user_table_insert = ("""
+INSERT INTO users (user_id, first_name, last_name, gender, level)
+    VALUES(%s, %s, %s, %s, %s)
+    ON CONFLICT (user_id)
+    DO UPDATE
+    SET level=EXCLUDED.level;
+""")
+
+song_table_insert = ("""
+INSERT INTO songs (song_id, title, artist_id, year, duration)
+    VALUES(%s, %s, %s, %s, %s)
+    ON CONFLICT (song_id)
+    DO NOTHING;
+""")
+
+artist_table_insert = ("""
+INSERT INTO artists (artist_id, name, location, latitude, longitude)
+    VALUES(%s, %s, %s, %s, %s)
+    ON CONFLICT (artist_id)
+    DO NOTHING;
+""")
+
+
+time_table_insert = ("""
+ INSERT INTO time (start_time, hour, day, week, month, year, weekday)
+    VALUES(to_timestamp(%s), %s, %s, %s, %s, %s, %s)
+    ON CONFLICT (start_time)
+    DO NOTHING;
+""")
+
+# FIND SONGS
+# get songid and artistid from song and artist tables
+song_select = ("""
+SELECT songs.song_id, artists.artist_id FROM 
+    (songs JOIN artists ON songs.artist_id=artists.artist_id)
+    WHERE songs.title=%s AND artists.name=%s AND songs.duration=%s;
+""")
+
+# QUERY LISTS
+
+create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
